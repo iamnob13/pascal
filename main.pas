e7a6107ff214b40c304bf7308446f0dca96cd6b8
ariya__bk <karykar83@gmail.com>
2026-02-01 21:08:46 +0330

    first commit

---
+++ main.pas
@@ -0,0 +1,33 @@
+program easy;
+var 
+ myscore : real;
+ f:text;
+   function ispassed(score: real):boolean;
+   begin
+       ispassed:= (score >= 10);
+    end;
+   procedure savetofile(score:real);
+      begin
+        assign(f,'test.pas');
+        append(f);
+        write(f,score:5:2);
+        writeln('saved');
+        close(f);
+      end;
+begin 
+ writeln('enter your score');
+ readln(myscore);
+if (myscore > 20) or (myscore < 0) then writeln('error')
+else 
+begin 
+ if ispassed(myscore) then 
+    begin
+      writeln('your pass');
+      savetofile(myscore);
+    end
+  else
+  begin
+    writeln('failed,not saved');
+  end;  
+  end;  
+end.
